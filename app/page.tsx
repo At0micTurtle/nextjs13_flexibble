036a0f24a2f15bfd063bcc6cdd517f89d8183e2b
At0micTurtle <carltrep@videotron.ca>
2023-08-22 11:48:35 -0400

    WIP Flexibble app

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import Image from 'next/image';
-
 export default function Home() {
   return (
-    <main>
-      <div className="underline text-3xl">
-        Hello
-      </div>
-    </main>
+    <section className="flexStart flex-col paddings mb-16">
+      <h1>Categories</h1>
+      <h1>Posts</h1>
+      <h1>Load More</h1>
+    </section>
   )
 };
